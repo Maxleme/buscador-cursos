e302713f32c1dbc0417a156ec497c2170298912d
Max <maxleme_21@hotmail.com>
2025-06-19 12:09:20 -0300

    Aula: php composer - criando um buscador de cursos

---
+++ buscar-cursos.php
@@ -0,0 +1,21 @@
+<?php 
+
+require 'vendor/autoload.php';
+require 'src/Buscador.php';
+
+use Alura\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(['base_uri' => 'https://www.alura.com.br/']);
+
+//debug: seguindo o exemplo proposto na aula o terminal não esta trazendo as informações, então gerei o html para verificar o q o request esta trazendo 
+//file_put_contents('alura.html', $html);
+
+$crawler = new Crawler();
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso) {
+    echo $curso. PHP_EOL;
+}
